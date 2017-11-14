698649af0e152c7ac10117f08f3ecd77dfca4dbe
Bryson Reynolds <bryson@brysonreynolds.com>
2017-11-14 01:32:57 -0600

    Renaming server => app and removing dependency on babel-node. Also optimized webpack for production

--- server.js
+++ server.js
@@ -1,42 +1,5 @@
-import path from 'path';
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import webpack from 'webpack';
-import webpackConfig from './webpack.config.js';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import express from 'express';
-import Html from './src/Html';
-import App from './src/App';
-
-const server = express();
-server.use('/public', express.static('public'));
-
-const initialData = {
-    owner: 'Bryson Reynolds'
-}
-
-const compiler = webpack(webpackConfig);
-
-server.use(webpackDevMiddleware(compiler, {
-    publicPath: webpackConfig.output.publicPath,
-    hot: true
-}));
-
-server.use(webpackHotMiddleware(compiler, {
-    reload: true
-}));
-
-// Root route
-server.get('/', (req, res) => {
-    ReactDOMServer.renderToNodeStream(
-        <Html initialData={JSON.stringify(initialData)}>
-            <App {...initialData} name="World" />
-        </Html>
-    ).pipe(res);
+require('babel-register')({
+    presets: [ 'env' ]
 });
 
-const listener = server.listen(3000, (data) => {
-    const app = listener.address();
-    console.log(`Server running at http://localhost:${app.port}`);            
-});
+require('./app.js');
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
+require('dotenv').config();
 var webpack = require('webpack');
 var path = require('path');
+
 
 var srcPath = path.join(__dirname, '/src')
 var distPath = path.join(__dirname, '/public');
 
-module.exports = {
-    entry: [
-        'react-hot-loader/patch',
-        'webpack-hot-middleware/client?noInfo=false',
-        path.join(srcPath, '/index.js')
-    ],
+console.log(process.env.NODE_ENV);
+
+const config = {
     module: {
         loaders: [
             {
@@ -19,11 +18,6 @@ module.exports = {
                 options: {
                     presets: ['react','env'],
                 }
-            }, 
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                loader: 'react-hot-loader/webpack'
             }
         ]
     },
@@ -37,7 +31,36 @@ module.exports = {
     },
     context: srcPath,
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
         new webpack.NamedModulesPlugin()
     ]
 };
+
+// Development Environment
+if (process.env.NODE_ENV === 'development') {
+    config.entry = [
+        'react-hot-loader/patch',
+        'webpack-hot-middleware/client?noInfo=false',
+        path.join(srcPath, '/index.js')
+    ]
+
+    config.module.loaders.push(
+        {
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'react-hot-loader/webpack'
+        }
+    )
+
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin()
+    )
+}
+
+// Production Environment
+if (process.env.NODE_ENV === 'production') {
+    config.entry = [
+        path.join(srcPath, '/index.js')        
+    ]
+}
+
+module.exports = config;
